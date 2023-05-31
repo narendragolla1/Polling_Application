a62ba107c21e2c060d4021e8c63f7b5d40d38a1d
narendragolla1 <116960868+narendragolla1@users.noreply.github.com>
2023-05-31 22:10:25 +0530

    added question view

--- poll/main/urls.py
+++ poll/main/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.Index.as_view(),name='index'),
+    path('question/<slug>',views.Question.as_view(),name='question'  )
 ]
--- poll/main/views.py
+++ poll/main/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.views.generic import (
-    ListView
+    ListView,
+    DetailView
 )
 from main import models
 # Create your views here.
 class Index(ListView):
     model=models.Question
     template_name='main/index.html'
+    
+class Question(DetailView):
+    model=models.Question
+    template_name='main/components/question.html'  
+    
